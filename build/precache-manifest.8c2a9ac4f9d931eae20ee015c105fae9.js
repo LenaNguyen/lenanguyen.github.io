bbdf9804abd21b273422b68348ac0c8e11657a72
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-01-09 08:53:36 -0500

    website v3

---
+++ build/precache-manifest.8c2a9ac4f9d931eae20ee015c105fae9.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fea6cc8346e6506c437f8c5f4020c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd31ec26e4105e52676f",
+    "url": "/static/css/main.2f057e6b.chunk.css"
+  },
+  {
+    "revision": "525f2f19af8c5477e65c",
+    "url": "/static/js/2.2b6c0fd1.chunk.js"
+  },
+  {
+    "revision": "fd31ec26e4105e52676f",
+    "url": "/static/js/main.036a2d8a.chunk.js"
+  },
+  {
+    "revision": "6fe9fcae5f392d77095d",
+    "url": "/static/js/runtime~main.191c029f.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "6fa8e36c5c6db51cdcb467bda149efdc",
+    "url": "/static/media/bg10-min.6fa8e36c.jpg"
+  },
+  {
+    "revision": "aa6936274e97c72c64b4c2b0755ea367",
+    "url": "/static/media/bg11-min.aa693627.jpg"
+  },
+  {
+    "revision": "5b0499058e4c26589015140050171d88",
+    "url": "/static/media/bg13-min.5b049905.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "6c655ceedc9e440e15741283406d94b2",
+    "url": "/static/media/bg5.6c655cee.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "500941036576ca778914b0e04dccfd37",
+    "url": "/static/media/bg8-min.50094103.jpg"
+  },
+  {
+    "revision": "fd8c746035b46d2bada38cb4dc4fce90",
+    "url": "/static/media/bg9-min.fd8c7460.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "96480d529db72cc8c7d838ea3bd00d9d",
+    "url": "/static/media/dancefest1-min.96480d52.jpg"
+  },
+  {
+    "revision": "07cf0dcb38ea80fe85416ffd6427fd1f",
+    "url": "/static/media/dancefest2-min.07cf0dcb.jpg"
+  },
+  {
+    "revision": "dfbf59f86227ed84f85bb50034b89c77",
+    "url": "/static/media/dancefest3-min.dfbf59f8.jpg"
+  },
+  {
+    "revision": "0c1931ca54a058f367cc6928543a19e1",
+    "url": "/static/media/dancefest4-min.0c1931ca.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "02bcecd553e22cc87b997d7e7b8d3cac",
+    "url": "/static/media/myndset1-min.02bcecd5.jpg"
+  },
+  {
+    "revision": "331f3e93f89cf0783dfb575e5f82f5e0",
+    "url": "/static/media/myndset2-min.331f3e93.jpg"
+  },
+  {
+    "revision": "3a2492267f2fce093231943a0504643d",
+    "url": "/static/media/myndset3-min.3a249226.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "bb6360e5c4602ca26cb13e72133b331a",
+    "url": "/static/media/recycleRight1-min.bb6360e5.jpg"
+  },
+  {
+    "revision": "b0f032f6491dbe58b59ece64b89973c9",
+    "url": "/static/media/safenet1-min.b0f032f6.jpg"
+  },
+  {
+    "revision": "27a3563ecf681dfca8357f68aa3781cd",
+    "url": "/static/media/safenet1.27a3563e.mp4"
+  },
+  {
+    "revision": "376b9dd776327d277fac7ef11c00ce2d",
+    "url": "/static/media/safenet2.376b9dd7.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "70c41ca6fc274ba74c5b444df52fce3f",
+    "url": "/static/media/smartbrain.70c41ca6.mp4"
+  },
+  {
+    "revision": "27e4bc03d9ef9cb712f2add02d9848c4",
+    "url": "/static/media/smartbrain1-min.27e4bc03.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "2eb8fa118e78a0c5b9225877346766e5",
+    "url": "/static/media/travelwise1-min.2eb8fa11.jpg"
+  },
+  {
+    "revision": "f94336edd0bb094ac412e27bd91cb6e0",
+    "url": "/static/media/travelwise2-min.f94336ed.jpg"
+  },
+  {
+    "revision": "e73c2f2c7b985f2f4c23676ea605abf9",
+    "url": "/static/media/travelwise3-min.e73c2f2c.jpg"
+  },
+  {
+    "revision": "fcbb1e451cc8f992458161f28940e1a2",
+    "url": "/static/media/travelwise4.fcbb1e45.jpg"
+  },
+  {
+    "revision": "8a91bf624e3169fb29a7d424b74f692a",
+    "url": "/static/media/waterlooRush1.8a91bf62.mp4"
+  }
+]);
