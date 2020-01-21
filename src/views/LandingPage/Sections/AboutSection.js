9b99fd71b80e4559f1e53c2926d5565b3a1391f7
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-01-20 22:55:13 -0500

    added resume

--- src/views/LandingPage/Sections/AboutSection.js
+++ src/views/LandingPage/Sections/AboutSection.js
@@ -47,7 +47,7 @@ export default function AboutSection() {
             id="resume"
             title="Resume"
             color="rose"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+            href="assets/Resume.pdf"
             target="_blank"
           >
             <Resume fontSize="large" />
